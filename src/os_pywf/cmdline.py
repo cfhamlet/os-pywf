ee830ecd20d2cecfa2af8bedd2e75220691980c8
Ozzy <cfhamlet@gmail.com>
2021-03-24 22:59:28 +0800

    add: base command line

---
+++ src/os_pywf/cmdline.py
@@ -0,0 +1,40 @@
+import click
+
+from os_pywf.utils import walk_modules
+
+from . import __version__
+
+
+class CommandFinder(click.MultiCommand):
+    def list_commands(self, ctx):
+        ctx.ensure_object(dict)
+        return list(self.__find_commnds(**ctx.obj).keys())
+
+    def get_command(self, ctx, name):
+        ctx.ensure_object(dict)
+        commands = self.__find_commnds(**ctx.obj)
+        return commands.get(name, None)
+
+    def __find_commnds(self, **kwargs):
+        command_packages = kwargs.get("command_packages", [])
+        commands = {}
+        for command_package in command_packages:
+            for cmd_module in walk_modules(command_package, skip_fail=False):
+                if hasattr(cmd_module, "cli") and isinstance(
+                    cmd_module.cli, click.Command
+                ):
+                    name = cmd_module.__name__.split(".")[-1]
+                    name = name.replace("_", "-")
+                    commands[name] = cmd_module.cli
+
+        return commands
+
+
+def execute(**kwargs):
+    @click.command(cls=CommandFinder, context_settings=dict(obj=kwargs))
+    @click.version_option(version=__version__)
+    @click.pass_context
+    def cli(ctx):
+        """Command line tool for os-pywf."""
+
+    cli()
---
+++ src/os_pywf/main.py
@@ -0,0 +1,6 @@
+from os_pywf.cmdline import execute
+
+
+def main():
+    command_packages = ["os_pywf.commands"]
+    execute(command_packages=command_packages)
